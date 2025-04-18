e0d36ef90c9c021e93b4e301730495037eb60240
Aurore Leblois <leblois-aurore@hotmail.fr>
2025-04-18 12:25:14 +0200

    simplier

--- src/Atoms/Button/Button.tsx
+++ src/Atoms/Button/Button.tsx
@@ -86,7 +86,7 @@ const Button: React.FC<ButtonProps> = ({
   const buttonCustom = styleButton(backgroundColor || '', shape, primary, size, disabled) as unknown as React.CSSProperties;
   const completeStyle: React.CSSProperties = { ...buttonCustom, ...sx };
   
-  let color =  backgroundColor ? backgroundColor : primary ? '#1ea7fd' : 'lightgray';
+  let color =  primary ? '#24c6f9' : 'lightgray';
   if (rippleColor) color = rippleColor;
   
   return (
